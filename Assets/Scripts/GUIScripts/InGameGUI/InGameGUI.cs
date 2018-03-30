d05b5eb1baafd146a211ad02adc1e5029e617db3
DESKTOP-QK0H43P\kanna <kannanjan21@gmail.com>
2018-03-30 21:13:23 +0530

    powerup fix

--- Assets/Scripts/GUIScripts/InGameGUI/InGameGUI.cs
+++ Assets/Scripts/GUIScripts/InGameGUI/InGameGUI.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.UI;
 
 public class InGameGUI : MonoBehaviour
@@ -8,27 +7,27 @@ public class InGameGUI : MonoBehaviour
     public static InGameGUI instance;
     public Text scoreText;
     public Text specialPointsText;
-	public Text highscoreText;
-	public Text TimerText;
-	public Image specialPointsImage;
-    
+    public Text highscoreText;
+    public Text TimerText;
+    public Image specialPointsImage;
 
-	public GameObject takenGUIDiamond;
-	public float TimeRemaining,startTime;
-	public int totalGameTime=120;
-	public Snake userSnake,opponentSnake; 
-	public GameObject[] powerSlots;
-	public Sprite Shield, Speed, Freeze, Health, Minishots, Heatseeker;
-	public PlayerPanelUI[] PlayerPanel;
-	public bool gameStarted;
+
+    public GameObject takenGUIDiamond;
+    public float TimeRemaining, startTime;
+    public int totalGameTime = 120;
+    public Snake userSnake, opponentSnake;
+    public GameObject[] powerSlots;
+    public Sprite Shield, Speed, Freeze, Health, Minishots, Heatseeker;
+    public PlayerPanelUI[] PlayerPanel;
+    public bool gameStarted;
 
 
     void Awake()
     {
         instance = this;
 
-	
-			
+
+
     }
 
     // Use this for initialization
@@ -36,72 +35,80 @@ public class InGameGUI : MonoBehaviour
     {
 
     }
-	public GameObject NetworkUI;
+    public GameObject NetworkUI;
     // Update is called once per frame
     void Update()
     {
-		//UpdateScoreText ();
-		//UpdateSpecialPointsText();
-		//UpdateHighScoreText();
-		UpdateTimer ();
-	
-		if (PhotonNetwork.GetPing () > 300)
-			NetworkUI.SetActive (true);
-		else
-			NetworkUI.SetActive (false);
+        //UpdateScoreText ();
+        //UpdateSpecialPointsText();
+        //UpdateHighScoreText();
+        UpdateTimer();
+
+        if (PhotonNetwork.GetPing() > 300)
+            NetworkUI.SetActive(true);
+        else
+            NetworkUI.SetActive(false);
     }
 
 
 
-	public void UpdateScoreText(){
-		string scoreString = ObliusGameManager.instance.TrimPercentage (((100 * (float)ScoreHandler.instance.score) / (float)GroundSpawner.instance.spawnedGroundPieces.Count).ToString ());
-		scoreText.text = "" + scoreString +  "%";
-	}
+    public void UpdateScoreText()
+    {
+        string scoreString = ObliusGameManager.instance.TrimPercentage(((100 * (float)ScoreHandler.instance.score) / (float)GroundSpawner.instance.spawnedGroundPieces.Count).ToString());
+        scoreText.text = "" + scoreString + "%";
+    }
 
-	public void UpdateSpecialPointsText(){
-		specialPointsText.text = ScoreHandler.instance.specialPoints.ToString();
-	}
+    public void UpdateSpecialPointsText()
+    {
+        specialPointsText.text = ScoreHandler.instance.specialPoints.ToString();
+    }
 
-	public void UpdateHighScoreText(){
-		string highscoreString =  ObliusGameManager.instance.TrimPercentage (((100 * (float)ScoreHandler.instance.highScore) / (float)GroundSpawner.instance.spawnedGroundPieces.Count).ToString());
-		highscoreText.text = "BEST SCORE: " + highscoreString + "%";
-	}
+    public void UpdateHighScoreText()
+    {
+        string highscoreString = ObliusGameManager.instance.TrimPercentage(((100 * (float)ScoreHandler.instance.highScore) / (float)GroundSpawner.instance.spawnedGroundPieces.Count).ToString());
+        highscoreText.text = "BEST SCORE: " + highscoreString + "%";
+    }
 
-	void UpdateTimer(){
-		if (SnakesSpawner.instance.playerSnake != null && SnakesSpawner.instance.playerSnake.isGameStarted && !gameStarted && (PhotonNetwork.connected && PhotonNetwork.room.PlayerCount==maxPlayers)) 
-		{
-			gameStarted = true;
-			TimeRemaining = totalGameTime;
-			PowerUpManager.instance.dontSpawn = false;
-		}
-			
-		if (gameStarted) 
-		{
-			gameStarted = true;
-			TimeRemaining = totalGameTime + startTime - Time.time;
-			if (TimeRemaining < 0) {
-				TimeRemaining = 0;
-				if (userSnake.ownedGroundPieces.Count > opponentSnake.ownedGroundPieces.Count) {
-					GUIManager.instance.gameOverGUI.OnWin ();
-					GUIManager.instance.gameOverGUI.Reason.text = userSnake.name + " covered more blocks than " + opponentSnake.name;
-					GUIManager.instance.ShowLog (userSnake.name + " covered more blocks than " + opponentSnake.name);
-				} else {
-					GUIManager.instance.gameOverGUI.OnLose ();
-					GUIManager.instance.gameOverGUI.Reason.text = opponentSnake.name + " covered more blocks than " + userSnake.name;
-					GUIManager.instance.ShowLog (userSnake.name + " covered more blocks than " + opponentSnake.name);
-				}
-				GUIManager.instance.ShowGameOverGUI ();
-			}
+    void UpdateTimer()
+    {
+        if (SnakesSpawner.instance.playerSnake != null && SnakesSpawner.instance.playerSnake.isGameStarted && !gameStarted && (PhotonNetwork.connected && PhotonNetwork.room.PlayerCount == maxPlayers))
+        {
+            gameStarted = true;
+            TimeRemaining = totalGameTime;
+            PowerUpManager.instance.dontSpawn = false;
+        }
 
-			TimerText.text = Mathf.RoundToInt (TimeRemaining).ToString ();	
-		}
-		else if (!gameStarted && (PhotonNetwork.connected)) 
-		{
-			TimerText.text = totalGameTime.ToString ();
-			PowerUpManager.instance.dontSpawn = true;
-		}
-	}
-	public int maxPlayers;
+        if (gameStarted)
+        {
+            gameStarted = true;
+            TimeRemaining = totalGameTime + startTime - Time.time;
+            if (TimeRemaining < 0)
+            {
+                TimeRemaining = 0;
+                if (userSnake.ownedGroundPieces.Count > opponentSnake.ownedGroundPieces.Count)
+                {
+                    GUIManager.instance.gameOverGUI.OnWin();
+                    GUIManager.instance.gameOverGUI.Reason.text = userSnake.name + " covered more blocks than " + opponentSnake.name;
+                    GUIManager.instance.ShowLog(userSnake.name + " covered more blocks than " + opponentSnake.name);
+                }
+                else
+                {
+                    GUIManager.instance.gameOverGUI.OnLose();
+                    GUIManager.instance.gameOverGUI.Reason.text = opponentSnake.name + " covered more blocks than " + userSnake.name;
+                    GUIManager.instance.ShowLog(userSnake.name + " covered more blocks than " + opponentSnake.name);
+                }
+                GUIManager.instance.ShowGameOverGUI();
+            }
+
+            TimerText.text = Mathf.RoundToInt(TimeRemaining).ToString();
+        }
+        else if (!gameStarted && (PhotonNetwork.connected))
+        {
+            TimerText.text = totalGameTime.ToString();
+            //PowerUpManager.instance.dontSpawn = true;
+        }
+    }
+    public int maxPlayers;
     public void OnTutorialButtonClick()
     {
         SoundsManager.instance.PlayMenuButtonSound();
@@ -118,74 +125,80 @@ public class InGameGUI : MonoBehaviour
 
 
 
-//	public void InstantiateTakeGUIObject(Vector3 pos){
-//		GameObject newObj = (GameObject)Instantiate (CollectedObject);
-//		newObj.transform.SetParent(transform,false);
-//		CollectedObject CO = newObj.GetComponent<CollectedObject> ();
-//		CO.rect.position = Camera.main.ScreenToWorldPoint (0,Screen.height,0);	
-//	}
+    //	public void InstantiateTakeGUIObject(Vector3 pos){
+    //		GameObject newObj = (GameObject)Instantiate (CollectedObject);
+    //		newObj.transform.SetParent(transform,false);
+    //		CollectedObject CO = newObj.GetComponent<CollectedObject> ();
+    //		CO.rect.position = Camera.main.ScreenToWorldPoint (0,Screen.height,0);	
+    //	}
 
 
 
-	public void InstantiateTakeGUIDiamond(Vector3 pos){
-		GameObject newObj = (GameObject)Instantiate (takenGUIDiamond);
-		newObj.transform.SetParent(transform,false);
-		TakenGUIDiamond newDiamond = newObj.GetComponent<TakenGUIDiamond> ();
-		newDiamond.rect.position = pos;	
-	}
+    public void InstantiateTakeGUIDiamond(Vector3 pos)
+    {
+        GameObject newObj = (GameObject)Instantiate(takenGUIDiamond);
+        newObj.transform.SetParent(transform, false);
+        TakenGUIDiamond newDiamond = newObj.GetComponent<TakenGUIDiamond>();
+        newDiamond.rect.position = pos;
+    }
 
 
-	public void EquipPowerup(){
-		for (int i = 0; i < powerSlots.Length; i++)
-			powerSlots [i].SetActive (false);
+    public void EquipPowerup()
+    {
+        for (int i = 0; i < powerSlots.Length; i++)
+            powerSlots[i].SetActive(false);
 
-		for (int i = 0; i < userSnake.loadedPowers.Count; i++) {
-			powerSlots [i].SetActive (true);
-			switch (SnakesSpawner.instance.playerSnake.loadedPowers [i]) {
+        for (int i = 0; i < userSnake.loadedPowers.Count; i++)
+        {
+            powerSlots[i].SetActive(true);
+            switch (SnakesSpawner.instance.playerSnake.loadedPowers[i])
+            {
 
-			case "Speed":
-				powerSlots [i].GetComponent <PowerSlot> ().selectedPower.sprite = Speed;
-				break;
+                case "Speed":
+                    powerSlots[i].GetComponent<PowerSlot>().selectedPower.sprite = Speed;
+                    break;
 
-			case "Shield":
-				powerSlots [i].GetComponent <PowerSlot> ().selectedPower.sprite = Shield;
-				break;
+                case "Shield":
+                    powerSlots[i].GetComponent<PowerSlot>().selectedPower.sprite = Shield;
+                    break;
 
-			case "Health":
-				powerSlots [i].GetComponent <PowerSlot> ().selectedPower.sprite = Health;
-				break;
+                case "Health":
+                    powerSlots[i].GetComponent<PowerSlot>().selectedPower.sprite = Health;
+                    break;
 
-			case "Heatseeker":
-				powerSlots [i].GetComponent <PowerSlot> ().selectedPower.sprite = Heatseeker;
-				break;
-			
-			case "3Shots":
-				powerSlots [i].GetComponent <PowerSlot> ().selectedPower.sprite = Minishots;
-				break;
+                case "Heatseeker":
+                    powerSlots[i].GetComponent<PowerSlot>().selectedPower.sprite = Heatseeker;
+                    break;
 
-			case "Freeze":
-				powerSlots [i].GetComponent <PowerSlot> ().selectedPower.sprite = Freeze;
-				break;
+                case "3Shots":
+                    powerSlots[i].GetComponent<PowerSlot>().selectedPower.sprite = Minishots;
+                    break;
 
-			case "Mine":
-				powerSlots [i].GetComponent <PowerSlot> ().selectedPower.sprite = Freeze;
-				break;
+                case "Freeze":
+                    powerSlots[i].GetComponent<PowerSlot>().selectedPower.sprite = Freeze;
+                    break;
+
+                case "Mine":
+                    powerSlots[i].GetComponent<PowerSlot>().selectedPower.sprite = Freeze;
+                    break;
 
 
-			}
-		}
-	}
+            }
+        }
+    }
 
-	public void SwitchPower(int selected){
-		string temp = userSnake.loadedPowers [0];
-		userSnake.loadedPowers [0] = userSnake.loadedPowers [selected];
-		userSnake.loadedPowers [selected] = temp;
+    public void SwitchPower(int selected)
+    {
+        string temp = userSnake.loadedPowers[0];
+        userSnake.loadedPowers[0] = userSnake.loadedPowers[selected];
+        userSnake.loadedPowers[selected] = temp;
 
-		EquipPowerup ();
-	}
+        EquipPowerup();
+    }
 
-	public void OnPowerUp(){
-		userSnake.UsePower ();
-	}
+    public void OnPowerUp()
+    {
+        userSnake.UsePower();
+    }
 
 }
--- Assets/_Scripts/PowerUpManager.cs
+++ Assets/_Scripts/PowerUpManager.cs
@@ -1,186 +1,126 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using DG.Tweening;
-public class PowerUpManager : MonoBehaviour {
+public class PowerUpManager : MonoBehaviour
+{
+    public static PowerUpManager instance;
+    public GameObject[] Powerups;
+    public List<GameObject> spawnedPowerups;
+    public float minInterval, maxInterval;
+    public bool dontSpawn;
 
-	public static PowerUpManager instance;
-	public GameObject[] Powerups;
-	public List<GameObject> spawnedPowerups;
-	public float minInterval,maxInterval;
-	public bool dontSpawn;
-	void Awake(){
-		instance = this;
-	}
+    void Awake()
+    {
+        instance = this;
+    }
 
-	public void StartSpawn () {
-		ClearPowerUps ();
-		NetworkClear ();
-		//SpawnRunePowerup ();
-		CancelInvoke ();
-		ClearPowerUpsLocal ();
-		Invoke ("SpawnPowerupLocal",5);
-	}
+    public void StartSpawn()
+    {
+        ClearPowerUps();
+        NetworkClear();
+        Invoke("StartSpawnTimer", 5);
+    }
 
-	public void StartNetworkPower(){
-		ClearPowerUps ();
-		NetworkClear ();
-		SpawnNetworkRunePowerup ();
-		Invoke ("StartSpawnTimer",5);
-	}
+    public void StartNetworkPower()
+    {
+        ClearPowerUps();
+        NetworkClear();
+        Invoke("StartSpawnTimer", 5);
+    }
 
-	void StartSpawnTimer()
-	{
-		dontSpawn = false;
-	}
+    void StartSpawnTimer()
+    {
+        dontSpawn = false;
+        if (!PhotonNetwork.inRoom)
+            SpawnRunePowerup();
+        else
+            SpawnNetworkRunePowerup();
+    }
 
-	public void StopSpawn(){
-		dontSpawn = true;
-		CancelInvoke ();
-	}
+    public void StopSpawn()
+    {
+        dontSpawn = true;
+        this.StopAllCoroutines();
+    }
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void SpawnRunePowerup()
+    {
+        if (!PhotonNetwork.inRoom)
+        {
+            if (!dontSpawn)
+            {
+                int Rand;
+                Rand = Random.Range(0, Powerups.Length);
+                GameObject Go;
+                Go = Instantiate(Powerups[Rand], transform);
+                Go.transform.localPosition = new Vector3(-4, 0, 0);
+                spawnedPowerups.Add(Go);
+                Rand = Random.Range(0, Powerups.Length);
+                Go = Instantiate(Powerups[Rand], transform);
+                Go.transform.localPosition = new Vector3(4, 0, 0);
+                spawnedPowerups.Add(Go);
+                Invoke("SpawnRunePowerup", 20);
+                Invoke("ClearPowerUps", 15);
+            }
+        }
+    }
 
-//	public void SpawnPowerup(){
-//
-//		ClearPowerUps ();
-//
-//		int Rand = Random.Range (0, Powerups.Length);
-//		GameObject Go = Instantiate (Powerups [Rand], transform);
-//		Go.transform.localPosition = new Vector3 (Random.Range (-4.5f, 4.5f), 0, Random.Range (-4.5f, 4.5f));
-//
-//		spawnedPowerups.Add (Go);
-//		Invoke ("SpawnPowerup",Random.Range (minInterval,maxInterval));
-//	}
+    public void SpawnNetworkRunePowerup()
+    {
+        if (PhotonNetwork.inRoom && PhotonNetwork.isMasterClient)
+        {
+            if (!dontSpawn)
+            {
+                int Rand;
+                Rand = Random.Range(0, Powerups.Length);
+                GameObject Go1;
+                GameObject Go2;
+                Go1 = PhotonNetwork.Instantiate("Powerups/" + Powerups[Rand].name, new Vector3(1.5f, -8, 0), Quaternion.identity, new byte());
+                spawnedPowerups.Add(Go1);
+                Rand = Random.Range(0, Powerups.Length);
+                Go2 = PhotonNetwork.Instantiate("Powerups/" + Powerups[Rand].name, new Vector3(14.5f, -8, 0), Quaternion.identity, new byte());
+                spawnedPowerups.Add(Go2);
+                Invoke("SpawnNetworkRunePowerup", 20);
+                Invoke("NetworkClear", 15);
+            }
+        }
+    }
 
-
-	public void SpawnPowerupLocal(){
-		int Rand;
-
-		Rand = Random.Range (0, Powerups.Length);
-		GameObject Go;
-		Go = Instantiate (Powerups [Rand], transform);
-		Go.transform.localPosition = new Vector3 (-4, 0, 0);
-		spawnedPowerups.Add (Go);
-
-		Rand = Random.Range (0, Powerups.Length);
-		Go = Instantiate (Powerups [Rand], transform);
-		Go.transform.localPosition = new Vector3 (4, 0, 0);
-
-		spawnedPowerups.Add (Go);
-
-		Invoke ("SpawnPowerupLocal", 20);
-		Invoke ("ClearPowerUpsLocal", 15);
-	}
-
-	public void ClearPowerUpsLocal(){
-		if (spawnedPowerups.Count<1)
-			return;
-
-
-		foreach (GameObject GO in spawnedPowerups.ToArray()) {
-			try {
-				spawnedPowerups.Remove (GO);
-				GO.transform.DOScale (Vector3.zero,0.4f).OnComplete (()=>{
-					Destroy (GO);
-				});
-
-			} catch {
-				print ("stupid bug");
-			}
-		}
-	}
+    public void ClearPowerUps()
+    {
+        if (spawnedPowerups.Count < 1)
+            return;
 
 
+        foreach (var powerup in spawnedPowerups.ToList())
+        {
+            RemovePowerUp(powerup);
+        }
+    }
 
-	public void SpawnRunePowerup(){
-		if (!PhotonNetwork.inRoom) {
-			if (!dontSpawn) {
-				ClearPowerUps ();
-				int Rand;
+    public void RemovePowerUp(GameObject GO)
+    {
+        spawnedPowerups.Remove(GO);
+        Destroy(GO);
+    }
 
-				Rand = Random.Range (0, Powerups.Length);
-				GameObject Go;
-				Go = Instantiate (Powerups [Rand], transform);
-				Go.transform.localPosition = new Vector3 (-4, 0, 0);
-				spawnedPowerups.Add (Go);
+    public void RemovePowerUpNetwork(GameObject GO)
+    {
 
-				Rand = Random.Range (0, Powerups.Length);
-				Go = Instantiate (Powerups [Rand], transform);
-				Go.transform.localPosition = new Vector3 (4, 0, 0);
+        spawnedPowerups.Remove(GO);
+        PhotonView.Get(GO).RPC("Destroy", PhotonTargets.MasterClient);
+    }
 
-				spawnedPowerups.Add (Go);
-			}
-			Invoke ("SpawnRunePowerup",20);
-			Invoke ("ClearPowerUps",15);
-		}
-	}
-
-
-
-
-
-	public void SpawnNetworkRunePowerup(){
-		if (PhotonNetwork.inRoom && PhotonNetwork.isMasterClient) 
-		{
-			if (!dontSpawn) 
-			{
-				NetworkClear ();
-				int Rand;
-				Rand = Random.Range (0, Powerups.Length);
-				GameObject Go1;
-				GameObject Go2;
-				if (PhotonNetwork.inRoom) {
-					Go1 = PhotonNetwork.Instantiate("Powerups/"+Powerups [Rand].name,new Vector3 (1.5f, -8, 0), Quaternion.identity, new byte());
-					spawnedPowerups.Add (Go1);
-					Rand = Random.Range (0, Powerups.Length);
-					Go2 = PhotonNetwork.Instantiate("Powerups/"+Powerups [Rand].name, new Vector3 (14.5f, -8, 0), Quaternion.identity, new byte());
-					spawnedPowerups.Add (Go2);
-				}
-			}
-			Invoke ("SpawnNetworkRunePowerup",20);
-			Invoke ("NetworkClear",15);
-		}
-	}
-
-	public void ClearPowerUps(){
-
-		if (spawnedPowerups.Count<1)
-			return;
-
-
-		foreach (GameObject GO in spawnedPowerups.ToArray()) {
-			try {
-				spawnedPowerups.Remove (GO);
-				Destroy (GO);
-			
-			} catch {
-				print ("stupid bug");
-			}
-		}
-	}
-
-	public void RemovePowerUp(GameObject GO){
-
-
-		spawnedPowerups.Remove (GO);
-		Destroy (GO);
-
-	}
-
-	public void RemovePowerUpNetwork(GameObject GO){
-
-		spawnedPowerups.Remove (GO);
-		PhotonView.Get (GO).RPC ("Destroy",PhotonTargets.MasterClient);
-	}
-
-	public void NetworkClear(){
-		PowerUpDestroy[] powerups = GameObject.FindObjectsOfType<PowerUpDestroy> ();
-		foreach(var p in powerups)
-			PhotonView.Get (p.gameObject).RPC ("Destroy",PhotonTargets.MasterClient);
-	}
+    public void NetworkClear()
+    {
+        if (!PhotonNetwork.isMasterClient)
+            return;
+        foreach (var powerup in spawnedPowerups.ToList())
+        {
+            if (spawnedPowerups.Count < 1)
+                return;
+            RemovePowerUpNetwork(powerup);
+        }
+    }
 
 }

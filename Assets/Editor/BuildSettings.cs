0fbf3f3dfed984468e35ecb903d011a58b5f4232
DESKTOP-QK0H43P\kanna <kannanjan21@gmail.com>
2018-04-16 18:04:45 +0530

    automatic build number updation

---
+++ Assets/Editor/BuildSettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEditor.Build;
+
+public class BuildSettings : IPreprocessBuild
+{
+    public int callbackOrder
+    {
+        get
+        {
+            return 0;
+        }
+    }
+
+    public void OnPreprocessBuild(BuildTarget target, string path)
+    {
+        float currentVersion = float.Parse(PlayerSettings.bundleVersion);
+        currentVersion += 0.01f;
+        PlayerSettings.bundleVersion = currentVersion.ToString();
+    }
+}
